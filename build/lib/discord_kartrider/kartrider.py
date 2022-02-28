129b70926ffd0011fc74669eddb12c1a5d366186
chunjoonnseo541 <joonseo123qa@gmail.com>
2022-02-28 17:23:54 +0900

    discord-kartrider 1.0.3

--- build/lib/discord_kartrider/kartrider.py
+++ build/lib/discord_kartrider/kartrider.py
@@ -4,25 +4,26 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import nextcord, requests, re
 
-async def kartideruser(ctx, api_key, file, name):
-    respone = requests.get(f"https://api.nexon.co.kr/kart/v1.0/users/nickname/{name}", headers={'Authorization': api_key})
-    ids = respone.json()["accessId"]
-    level = respone.json()["level"]
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(file, options=chrome_options)
-    driver.get(f"https://tmi.nexon.com/kart/user?nick={name}")
-    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="inner"]/div[1]/div[2]/div[2]/div[2]/span[1]'))).click()
-    licenses = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="inner"]/div[1]/div[2]/div[2]/h1/span'))).value_of_css_property("background-image")
-    image = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="inner"]/div[1]/div[2]/div[1]/img'))).get_attribute('src')
+async def kartideruser(ctx, api_key, name, file=None):
+    if file==None:
+        driver = webdriver.Chrome(options=webdriver.ChromeOptions().add_argument("--headless"))
+        return
+    driver = webdriver.Chrome(file, options=webdriver.ChromeOptions().add_argument("--headless"))
     driver.get(f"https://bazzi.gg/rider/{name}")
     ranking = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div/div/div[3]/div[2]/div/div/div[2]/div/p[2]'))).text
     many_track = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div/div/div[3]/div[2]/div/div/div[3]/div/p[2]'))).text
     many_drive = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div/div/div[3]/div[2]/div/div/div[4]/div/p[2]'))).text
-    winningrate=WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div/div/div[3]/div[2]/div/div/div[1]/div/p[2]'))).text
+    winningrate = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div/div/div[3]/div[2]/div/div/div[1]/div/p[2]'))).text
     winner=WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div/div/div[3]/div[2]/div/div/div[1]/div/p[3]/span[1]'))).text
     loseing=WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div/div/div[3]/div[2]/div/div/div[1]/div/p[3]/span[2]'))).text
     licensestext=WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/main/div/div/div[1]/nav/div/div[2]/div/div[2]/span'))).text
+    respone = requests.get(f"https://api.nexon.co.kr/kart/v1.0/users/nickname/{name}", headers={'Authorization': api_key})
+    ids = respone.json()["accessId"]
+    level = respone.json()["level"]
+    driver.get(f"https://tmi.nexon.com/kart/user?nick={name}")
+    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="inner"]/div[1]/div[2]/div[2]/div[2]/span[1]'))).click()
+    licenses = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="inner"]/div[1]/div[2]/div[2]/h1/span'))).value_of_css_property("background-image")
+    image = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="inner"]/div[1]/div[2]/div[1]/img'))).get_attribute('src')
     driver.quit()
     embed = nextcord.Embed(title="카트 유저검색",description=f"{name}님의 라이더 정보", color=0x00FFFF)
     embed.add_field(name="고유 ID", value=ids, inline=True)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(name='discord_kartrider', # 패키지 명
 
-    version='1.0.2',
+    version='1.0.3',
 
     long_description = long_description,
 
